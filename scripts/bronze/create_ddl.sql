bebe58f9caa897dff554fb8c4bd24a298e2847c7
teo-mal <tmalezidis@gmail.com>
2025-04-01 20:25:25 +0300

    u

--- scripts/bronze/create_ddl.sql
+++ scripts/bronze/create_ddl.sql
@@ -17,20 +17,20 @@ client_marital_status NVARCHAR(50),
 client_gender NVARCHAR(50),
 create_date DATE,
 end_date DATE,
-country NVARCHAR(50),
+country NVARCHAR(50)
 );
 
 DROP TABLE IF EXISTS bronze.crm_prds_info;
 CREATE TABLE bronze.crm_prds_info(
 product_id NVARCHAR(50),
 product_type NVARCHAR(50),
-product_name NVARCHAR(150),
+product_name NVARCHAR(150)
 );
 
 DROP TABLE IF EXISTS bronze.crm_branch_cnt_loc;
 CREATE TABLE bronze.crm_branch_cnt_loc(
 branch NVARCHAR(50),
-client_id INT,
+client_id INT
 );
 
 
@@ -41,7 +41,7 @@ client_id INT,
 product_id NVARCHAR(50),
 transaction_date DATETIME,
 invested_amount  NVARCHAR(50),--some values have the $ except from INT--
-withdrawal_amount NVARCHAR(50),--some values have the $ except from INT--
+withdrawal_amount NVARCHAR(50)--some values have the $ except from INT--
 );
 
 DROP TABLE IF EXISTS bronze.erp_HR_data;
@@ -54,17 +54,17 @@ department NVARCHAR(50),
 position NVARCHAR(50),
 hiring_date DATE,
 end_date DATE,
-salary INT,
+salary INT
 );
 
 DROP TABLE IF EXISTS bronze.erp_research_data;
 CREATE TABLE bronze.erp_research_data(
 employee_id NVARCHAR(50),
-product_id NVARCHAR(50),
+product_id NVARCHAR(50)
 );
 
 DROP TABLE IF EXISTS bronze.erp_branch_emp_loc;
 CREATE TABLE bronze.erp_branch_emp_loc(
 branch NVARCHAR(50),
-employee_id NVARCHAR(50),
+employee_id NVARCHAR(50)
 );
